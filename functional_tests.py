7f790f2d80710e4cc3f0ba65ad076cb0d4fadb26
ammarharara95 <sasuke569@gmail.com>
2018-01-17 07:17:32 +0200

    Front page HTML is generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import unittest
-
+from selenium.webdriver.common.keys import Keys
+import time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -12,13 +13,12 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1')
-        self.assertIn('To-Do',header_text)
+        self.assertIn('To-Do',header_text.text)
 
         inputbox = self.browser.find_element_by_id('id_inputbox_todo')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+            'Enter a to-do item'        )
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
@@ -26,7 +26,9 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+                    'New to-do item did not appear in table'
+            
         )
         self.fail('Finish the test!')
 
